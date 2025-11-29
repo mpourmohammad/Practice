a60091d2ce9d5c28206d3b4d492050d73f667980
m.pourmohammad74 <m.pourmohammad74@gmail.com>
2025-11-29 13:10:36 +0330

    calculate Balance

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
     final static byte MONTHS_IN_YEAR = 12;
     final static byte PRECENT = 100;
 
-
     public static void main(String[] args) {
         int principal = (int) readNumber("Principal: ", 1000, 1_000_000);
         float annualInterest = (float) readNumber("Annual Interest Rate: ", 1, 30);
